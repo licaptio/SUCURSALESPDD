d1a12acb2777a7aa0415601bcceb5b994882020e
licaptio <licaptio42@gmail.com>
2026-05-30 09:48:30 -0600

    Create sucursal-config.js

---
+++ ALLENDE2/sucursal-config.js
@@ -0,0 +1,32 @@
+export const CONFIG_SUCURSAL = {
+  SUCURSAL: "ALLENDE 1",
+  RUTA_TRANSFERENCIAS: "transferencias1",
+
+  TITULO_APP: "SOLICITUD DE TRANSFERENCIA",
+  TITULO_NAVEGADOR: "Transferencia Solicitud - PROVEEDORA ALLENDE",
+  SUCURSAL_HEADER: "ALLENDE 1",
+
+  LOGO: "logo.jfif",
+
+  PREFIJO_FOLIO: "SOL",
+
+  FIREBASE: {
+    RAIZ_TIENDAS: "TIENDAS",
+    SUCURSAL: "ALLENDE 1",
+    COLECCION_TRANSFERENCIAS: "transferencias1",
+    COLECCION_PRODUCTOS: "productos",
+    COLECCION_FOTOS_META: "productos_fotos_meta"
+  },
+
+  INDEXEDDB: {
+    DB_NAME: "PROVSOFT_PDD_MOVIL_ALLENDE_1",
+    DB_VERSION: 3,
+    STORE_PRODUCTOS: "catalogo_productos_completo",
+    STORE_META: "metadata",
+    STORE_FOTOS_META: "productos_fotos_meta"
+  },
+
+  CLAVES: {
+    ULTIMA_DESCARGA_CATALOGO: "ultima_descarga_catalogo"
+  }
+};

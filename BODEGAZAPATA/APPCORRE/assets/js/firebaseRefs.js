8eadc5b651dc511513212ae82d94635311c68812
licaptio <licaptio42@gmail.com>
2026-06-27 17:53:29 -0600

    Delete BODEGAZAPATA/APPCORRE directory

--- BODEGAZAPATA/APPCORRE/assets/js/firebaseRefs.js
+++
@@ -1,6 +0,0 @@
-import { db } from "./config.js";
-import { collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-export const REF_ENTRADAS = collection(db, "almacenes", "almacen_zapata", "entradas");
-export const REF_SALIDAS = collection(db, "almacenes", "almacen_zapata", "salidas1.0");
-export const REF_AJUSTES = collection(db, "almacenes", "almacen_zapata", "ajustes_inventario");

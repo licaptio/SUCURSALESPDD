ec22ad028dbfc638e9e713e197ed6e08af4efbfa
licaptio <licaptio42@gmail.com>
2026-07-17 11:07:53 -0600

    Add files via upload

---
+++ ALLENDEONE/CATAINGE/config.js
@@ -0,0 +1,9 @@
+// Coloca aquí la configuración de tu proyecto Firebase.
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};

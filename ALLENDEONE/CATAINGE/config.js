29c5af310375701eddd00f540b31cec603fe4608
licaptio <licaptio42@gmail.com>
2026-07-18 18:43:49 -0600

    Delete ALLENDEONE/CATAINGE directory

--- ALLENDEONE/CATAINGE/config.js
+++
@@ -1,9 +0,0 @@
-// Coloca aquí la configuración de tu proyecto Firebase.
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};

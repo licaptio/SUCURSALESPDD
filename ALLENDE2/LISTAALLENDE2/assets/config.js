264ca0aec22ac707be348babc5f38e898cf1491b
licaptio <licaptio42@gmail.com>
2026-08-02 22:55:29 -0600

    Add files via upload

---
+++ ALLENDE2/LISTAALLENDE2/assets/config.js
@@ -0,0 +1,16 @@
+export const APP_CONFIG = Object.freeze({
+  tienda: "ALLENDE 2",
+  tiendaContraparte: "ALLENDE 1",
+  colecciones: Object.freeze({
+    inventarios: "inventario1",
+    ventas: "ventas",
+    transferencias: "SURTIDOTRANS",
+    ajustes: "ajustes_inventario",
+    devoluciones: "DEVOLUCION",
+    intercambiosEntrada: "intercambios",
+    intercambiosSalida: "intercambios2",
+    mermas: "MERMA",
+    equivalencias: "configuracion_equivalencias",
+    conteoDiario: "CONTEO_DIARIO"
+  })
+});

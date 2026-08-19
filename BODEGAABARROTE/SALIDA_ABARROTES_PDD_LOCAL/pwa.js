98df37af38a2fe589ce07bb8725bd6dc1e5037f6
licaptio <licaptio42@gmail.com>
2026-08-19 07:38:53 -0600

    Delete BODEGAABARROTE/SALIDA_ABARROTES_PDD_LOCAL directory

--- BODEGAABARROTE/SALIDA_ABARROTES_PDD_LOCAL/pwa.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js")
-      .then(() => console.log("Service Worker registrado"))
-      .catch((err) => console.error("Error registrando Service Worker:", err));
-  });
-}

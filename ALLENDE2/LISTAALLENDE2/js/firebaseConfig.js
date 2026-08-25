fcb9d102682019da47cb77e83b5bd2db20147316
licaptio <licaptio42@gmail.com>
2026-08-24 20:34:45 -0600

    Add files via upload

---
+++ ALLENDE2/LISTAALLENDE2/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
+};

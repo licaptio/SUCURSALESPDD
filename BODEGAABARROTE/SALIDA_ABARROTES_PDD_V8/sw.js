ba94676daf56ebb96dfabc639a0c9fcff650cac6
licaptio <licaptio42@gmail.com>
2026-08-31 10:30:28 -0600

    Delete BODEGAABARROTE/SALIDA_ABARROTES_PDD_V8 directory

--- BODEGAABARROTE/SALIDA_ABARROTES_PDD_V8/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='abarrotes-pdd-v8';
-const LOCAL=['./','./index.html','./manifest.webmanifest','./assets/styles.css','./assets/app.js','./assets/firebase-config.js','./assets/telegram-config.js','./assets/logo.jfif','./assets/bodega-bg.webp','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r&&r.ok&&new URL(e.request.url).origin===location.origin){const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

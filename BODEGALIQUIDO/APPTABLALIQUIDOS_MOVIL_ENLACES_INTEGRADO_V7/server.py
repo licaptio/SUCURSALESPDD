108a1573405db72a499f9f93e9cae1c6cb0c6e3e
licaptio <licaptio42@gmail.com>
2026-09-01 07:26:39 -0600

    Delete BODEGALIQUIDO/APPTABLALIQUIDOS_MOVIL_ENLACES_INTEGRADO_V7 directory

--- BODEGALIQUIDO/APPTABLALIQUIDOS_MOVIL_ENLACES_INTEGRADO_V7/server.py
+++
@@ -1,7 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-import os
-
-PORT = 8000
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-print(f"PROVSOFT Enlace Móvil: http://localhost:{PORT}")
-ThreadingHTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler).serve_forever()

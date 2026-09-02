7876d12d4cdbeaa5bae094f24c8677f249147928
licaptio <licaptio42@gmail.com>
2026-09-02 17:03:21 -0600

    Delete BODEGAABARROTE/SALIDA_ABARROTES_PDD_V2 directory

--- BODEGAABARROTE/SALIDA_ABARROTES_PDD_V2/FIREBASE_DEPLOY/02_LOGIN_FIREBASE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-where firebase >nul 2>nul || (echo Ejecuta 00_INSTALAR_FIREBASE_CLI.bat primero.& pause & exit /b 1)
-firebase login
-pause
--- BODEGAABARROTE/SALIDA_ABARROTES_PDD_V2/scripts/03_ABRIR_URL_FIREBASE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-if not exist ".firebase_site_id.txt" (
-  echo Primero ejecuta 01_PREPARAR_HOSTING_NUEVO.bat
-  pause
-  exit /b 1
-)
-set /p SITE_ID=<".firebase_site_id.txt"
-start "" "https://%SITE_ID%.web.app"
